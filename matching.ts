44c01b8997780fcee1bd0d61ae801b64470cd7a1
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-07-24 00:30:14 +0300

    Create matching.ts

---
+++ matching.ts
@@ -0,0 +1,20 @@
+function naivsar(text: string, pattern:string): number[]{
+  const result: number[]=[];
+  const n = text.length;
+  const m = patter.length;
+
+  for(let i =0; i< n - m; i++){
+    let j =0;
+    while(j<m && text[i + j] ===pattern[]){
+      j+++;
+    }
+    if(j === m){
+      rsult.push(i);
+    }
+  }
+  return result;
+}
+
+const text = "arhjecdn";
+const pattern = "arhj";
+console.log(naivsar(text,pattern))//output:[]
